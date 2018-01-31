2beb30a7daa0d866aea6d566160c7e0132a4d340
Michael Zimmerman <Mac-Desktop@Michaels-MacBook-Pro.local>
2018-01-30 19:27:13 -0600

    add snippets and update images

---
+++ code_snippet/array.js
@@ -0,0 +1,21 @@
+
+// example of a simple array
+var favoriteFoods = ["pizza", "steak", "burger"];
+
+
+// example of array wiht multiple data types
+var menu = [
+    "pizza",
+    [10, 12, 15],
+    [true, true, false],
+    "this array makes no sense!"
+];
+
+// forEach example
+favoriteFoods.forEach (function (item, index, array) {
+    console.log (item, index);
+})
+// pizza 0
+// steak 1
+// burger 2
+
